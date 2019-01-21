cb3d995ed2c0c1fdbdda042344b9fcb44d8e37f5
Amalie Smidth <amaliesmidth@gmail.com>
2019-01-21 22:56:10 +0000

    included tty prompt in relevant specs

--- spec/mission_control_spec.rb
+++ spec/mission_control_spec.rb
@@ -1,3 +1,4 @@
+require 'tty/prompt'
 require 'mission_control.rb'
 
 describe MissionControl do
--- spec/styling_spec.rb
+++ spec/styling_spec.rb
@@ -1,16 +1,17 @@
-# require 'styling.rb'
-#
-# describe Styling do
-#
-#   let (:PROMPT) { TTY::Prompt.new }
-#
-#   describe "#puts_fast" do
-#     context "when given a string as input" do
-#       it "prints each character to the console at a 0.02 second interval" do
-#         expect(puts_fast("hello there")).to eq "hello there\n"
-#       end
-#     end
-#   end
-#
-#
-# end
+require 'tty/prompt'
+require 'styling.rb'
+
+describe Styling do
+
+  let (:PROMPT) { TTY::Prompt.new }
+
+  describe "#puts_fast" do
+    context "when given a string as input" do
+      it "prints each character to the console at a 0.02 second interval" do
+        expect(puts_fast("hello there")).to eq "hello there\n"
+      end
+    end
+  end
+
+
+end

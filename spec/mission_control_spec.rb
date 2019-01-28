e81abe6d48f241e96ead1e1fbf5f50e531cf30f9
Alfredo Motta <mottalrd@users.noreply.github.com>
2019-01-28 07:18:24 +0000

    Update mission_control_spec.rb

--- spec/mission_control_spec.rb
+++ spec/mission_control_spec.rb
@@ -1,14 +1,33 @@
 require 'tty/prompt'
 require 'mission_control.rb'
 
+# It seems to me that this class is literally the entry point of the program
+# so the way I would expect to use it if I was in a repl would be
+# MissionControll.new.begin
+# After this I wouldn't probably refer to the MissionControl instance
+# and just follow the game.
+# If that's the case, the I would test a couple of simple input/output
+# interactions with the game.
+# You can do that by stubbing `gets` and return the various strings that
+# the user would input
+# allow(mission).to receive(:gets).and_return(["Yes", "N", etc...])
 describe MissionControl do
 
   let(:mission) { MissionControl.new }
 
   describe "#begin" do
     context "when choice is yes" do
+      # I would use let for this
       x_coord = "5"
       y_coord = "5"
+      
+      # I am not sure about these tests
+      # To me a more helpful test would be
+      # context 'when I choose yes' do
+      #   it 'prints a message to the screen asking for the coordinates (?)'
+      # end
+      # 
+      # how it is done (i.e. via instance variable and Plateau) could be hidden 
       it "should create a new instance, @initialized_plateau, of the Plateau class based on the user's input" do
         expect()
       end
@@ -29,6 +48,7 @@ describe MissionControl do
     end
   end
 
+  # Is get_input a public or private method? 
   describe "#get_input" do
     # it "should should receive user input and assign it to @starting_x_coord" do
     #   expect()
@@ -47,6 +67,7 @@ describe MissionControl do
     # end
   end
 
+  # Is this a public or private method?
   describe "#validate_instructions" do
     context "@instructions only consist of 'L' 'R' 'M' characters" do
       it "should call deploy_rover method" do
@@ -63,6 +84,7 @@ describe MissionControl do
     end
   end
 
+  # Is this a public or private method?
   describe "#deploy_rover" do
     context "when choice is yes" do
       it "should call the get_input method" do
@@ -76,6 +98,7 @@ describe MissionControl do
     end
   end
 
+  # Is this a public or private method?
   describe "#trigger_action" do
     it "should display the initial position(s) of the rover(s) on the grid plateau" do
       expect()
